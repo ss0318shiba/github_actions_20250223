1014e1fa8d15ac28cc41a69a9abcbf7fa89b6dc7
ss0318shiba <ss0318shiba@gmail.com>
2025-02-23 19:55:49 +0900

    Update test_main.py

--- project/tests/test_main.py
+++ project/tests/test_main.py
@@ -6,4 +6,4 @@ class MainTestCase(TestCase):
     def test_add(self):
         self.assertEqual(5, function(2, 3))
     def test_add_false(self):
-        self.assertEqual(6, function(2, 3))
+        self.assertEqual(6, function(3, 3))
