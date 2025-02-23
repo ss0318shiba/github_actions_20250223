0a6e28be15567da6b3a9529998e0658af5a870cc
ss0318shiba <ss0318shiba@gmail.com>
2025-02-23 19:22:43 +0900

    Update main.py

--- project/src/main.py
+++ project/src/main.py
@@ -1,16 +1,2 @@
-import sys
-
-
-def add(a: int, b: int) -> int:
-    return a + b
-
-
-def main():
-    if len(sys.argv) <= 2:
-        print("a and b is required")
-    _, a, b, *_ = sys.argv
-    print(add(*map(int, (a, b))))
-
-
-if __name__ == "__main__":
-    main()
+print('hello world');
+def function(a, b): return(a+b);
